47827b989f7559f4549f59fb18859eb9e2a15120
root <pagubata@yahoo.com>
2023-02-21 09:50:07 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main fuction
+ * _putchar: print the alphabets putchar
+ * Return: Always 0
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
