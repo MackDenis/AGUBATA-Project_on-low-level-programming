41c6265bc261b56bd0a573c83886a3a81a58e8c8
root <pagubata@yahoo.com>
2023-04-10 02:24:23 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary representation of a number.
+ * @n: unsigned long int.
+ * Return: there is no return.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+	int flag = 0;
+	while (mask)
+	{
+		if (n & mask)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+
+		else if (flag)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
+	if (!flag)
+		_putchar('0');
+}
